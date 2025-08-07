5ea26be10a91231100605723d43e3780824fccc3
Plycedes <abhaypratapyadav340@gmail.com>
2025-08-08 00:01:49 +0530

    register WatchList model with customized admin interface

--- backend/watchlist/admin.py
+++ backend/watchlist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WatchList
 
-# Register your models here.
+@admin.register(WatchList)
+class WatchListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company', 'created_at')
+    list_filter = ('created_at', 'company')
+    search_fields = ('user__username', 'company__company_name')
+    ordering = ('-created_at',)
